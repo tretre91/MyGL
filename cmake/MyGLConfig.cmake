38d3e62b62568005019d1eb8b7960bf0a8dd9c60
Trevis Morvany <trevis.morvany@u-psud.fr>
2021-04-24 15:26:12 +0200

    Deleted sphinx find module, fixed typo in config file

--- cmake/MyGLConfig.cmake
+++ cmake/MyGLConfig.cmake
@@ -1,4 +1,4 @@
-include(CMakeFindDependancyMacro)
-find_dependancy(SDL2)
-find_dependancy(Freetype 2)
+include(CMakeFindDependencyMacro)
+find_dependency(GLFW3)
+find_dependency(Freetype 2)
 include("${CMAKE_CURRENT_LIST_DIR}/MyGLTargets.cmake")
