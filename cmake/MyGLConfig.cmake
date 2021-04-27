aa6c8f80372a2141839e85801c16d4251b93badc
Trevis Morvany <trevis.morvany@u-psud.fr>
2021-04-27 14:56:34 +0200

    Changes relative to the dependencies - On unix shared libs are used when the MYGL_SHARED target is enabled - Added hints to the file_dependency calls in the config file - Changed the install interface include directories

--- cmake/MyGLConfig.cmake
+++ cmake/MyGLConfig.cmake
@@ -1,4 +1,4 @@
 include(CMakeFindDependencyMacro)
-find_dependency(GLFW3)
-find_dependency(Freetype 2)
+find_dependency(GLFW3 PATHS "../glfw3")
+find_dependency(Freetype 2 PATHS "../freetype")
 include("${CMAKE_CURRENT_LIST_DIR}/MyGLTargets.cmake")
