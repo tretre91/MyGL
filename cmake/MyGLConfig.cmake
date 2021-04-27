5c89527bd898ed2e35fb11e6b6143a8ab6842db5
trevis <trevismorvany@gmail.com>
2021-04-27 20:37:36 +0200

    Now using CPM.cmake for dependency management

--- cmake/MyGLConfig.cmake
+++ cmake/MyGLConfig.cmake
@@ -1,4 +1,4 @@
 include(CMakeFindDependencyMacro)
-find_dependency(GLFW3 PATHS "../glfw3")
+find_dependency(glfw3 PATHS "../glfw3")
 find_dependency(Freetype 2 PATHS "../freetype")
 include("${CMAKE_CURRENT_LIST_DIR}/MyGLTargets.cmake")
